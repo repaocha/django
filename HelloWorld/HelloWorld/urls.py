ac6a85dee3f7e61954ed1195bba671b84af3d73e
admin <admin@localhost.localdomain>
2017-07-24 17:02:02 +0800

    django

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from . import view,testdb,search
 from django.contrib import admin
  
 urlpatterns = [
+    url(r'^search-form$',search.search_form),
+    url(r'^search$',search.search),
+    url(r'^testdb$',testdb.testdb),
     url(r'^admin/', admin.site.urls),
 ]

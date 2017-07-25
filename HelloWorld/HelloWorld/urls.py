c78c32dbd323b3ad55c200a5481003ca907dd8f9
admin <1029804941@qq.com>
2017-07-25 17:05:31 +0800

    django

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from . import view
 from . import view,testdb,search
 from django.contrib import admin
  
 urlpatterns = [
+    url(r'^hello$',view.hello),
     url(r'^search-form$',search.search_form),
     url(r'^search$',search.search),
     url(r'^testdb$',testdb.testdb),
--- HelloWorld/HelloWorld/view.py
+++ HelloWorld/HelloWorld/view.py
@@ -2,7 +2,7 @@
 #  -*-coding:utf-8 -*-
 
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render #增加render代替HttpResponse,用于向模板提交数据
 
 def hello(request):
 	context    ={}

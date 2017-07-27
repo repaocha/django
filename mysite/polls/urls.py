d3de0df1fbc9fd657ff44c8d0c20b958d10d04b7
admin <1029804941@qq.com>
2017-07-27 17:16:11 +0800

    django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),     #ex:/polls/
-    url(r'^(?p<question_id>[0-9]+)/$',views.detail,name='detail'),   #ex:/polls/5/
-    url(r'^(?p<question_id>[0-9]+)/results/$',views.results,name='result'),  #ex:/polls/5/results/
-    url(r'^(?p<question_id>[0-9]+)/vote/$',views.vote,name='vote'),   #ex:/polls/5/vote/    
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),   #ex:/polls/5/
+    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='result'),  #ex:/polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),   #ex:/polls/5/vote/    
 ]

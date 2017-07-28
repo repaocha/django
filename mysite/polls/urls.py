13caf646abfea8d832f8ffcc705b289bea0e9af8
admin <1029804941@qq.com>
2017-07-28 17:50:30 +0800

    django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,11 +3,18 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-    url(r'^$',views.index,name='index'),     #ex:/polls/
-    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),   #ex:/polls/5/
-    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='result'),  #ex:/polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),   #ex:/polls/5/vote/    
-]
+#urlpatterns=[
+#   url(r'^$',views.index,name='index'),     #ex:/polls/
+#    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),   #ex:/polls/5/
+#    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='result'),  #ex:/polls/5/results/
+#    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),   #ex:/polls/5/vote/    
+#]
 
 #?P<question_id> 定义一个名字,它将用于标识匹配的模式;[0-9]+是匹配一串数字的正则表达式.
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,14 @@
 from django.shortcuts import  get_object_or_404, render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from . models import Question
+from django.views import generic
 from django.http import Http404
+from django.http import HttpResponseRedirect,HttpResponse
+from django.core.urlresolvers import reverse
+
+from .models import Choice, Question
+
+#from . models import Choice,Question
 
 # Create your views here.
 
@@ -21,11 +27,12 @@ from django.http import Http404
 #    return HttpResponse(template.render(context))
 #载入polls/index.html模板,并传给它一个context.Context是一个字典,将模板变量的名字映射到Python 对象.
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+#def index(request):
+#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	context = {'latest_question_list': latest_question_list}
+#	return render(request, 'polls/index.html', context)
 
+#视图
 #def detail(request,question_id):
 #	return HttpResponse("You're looking at question%s." % question_id)
 
@@ -36,15 +43,85 @@ def index(request):
 #		raise Http404("Question does not exist"
 #	return render(request,'polls/detail.html',{'question':question})
 
-def detail(request,question_id):
-	question=get_object_or_404(Question,pk=question_id)
-	return render(request,'polls/detail.html',{'question':question})
+#def detail(request,question_id):
+#	question=get_object_or_404(Question,pk=question_id)
+#	return render(request,'polls/detail.html',{'question':question})
 #get_object_or_404() 函数将模型Question作为它的第一个参数，任意数量的关键字参数作为它的第二个参数，
 #它会将这些关键字参数传递给模型管理器中的get() 函数,如果对象不存在，引发一个 Http404异常.
 
-def results(request,question_id):
-	response="You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+#视图
+#def results(request,question_id):                         
+#	response="You're looking at the results of question %s."
+#	return HttpResponse(response % question_id)
+
+#def results(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/results.html', {'question': question
+
+#def vote(request,question_id):
+#	return HttpResponse("You're voting on question %s." % question_id)
+
+#def vote(request, question_id):
+#	p = get_object_or_404(Question, pk=question_id)
+#	try:
+#		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+#	except (KeyError, Choice.DoesNotExist):
+#	# Redisplay the question voting form.
+#	return render(request, 'polls/detail.html', {
+#		'question':p,
+#		'error_message': "You didn't select a choice.",
+#		})
+#else:
+#	selected_choice.votes += 1
+#	selected_choice.save()
+#	return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+#request.POST 是一个类似字典的对象，让你可以通过关键字的名字获取提交的数据.request.POST的值永远是字符串.
+#request.POST['choice'] 以字符串形式返回选择的Choice的ID,如果在POST数据中没有提供choice，request.POST['choice']将引发一个KeyError.
+#try...except代码检查KeyError，如果没有给出choice将重新显示Question表单和一个错误信息。
+#增加Choice的得票数之后，返回一个 HttpResponseRedirect而不是常用的HttpResponse。HttpResponseRedirect只接收一个参数：用户将要被重定向的URL.
+#成功处理POST数据后总是返回一个HttpResponseRedirect.
+#reverse()函数避免了在视图函数中硬编码URL,它需要我们给出我们想要跳转的视图的名字和该视图所对应的URL模式中需要给该视图提供的参数.
+#reverse() 调用将返回一个这样的字符串:'/polls/3/results/',3是p.id的值,重定向的URL将调用'results'视图来显示最终的页面.
+#request是一个HttpRequest对象.
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request,question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	p=get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice=p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError,Choice.DoesNotExist):
+		return render(request,'polls/detail.html',{
+			'question':p,
+			'error_message':"You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes +=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
